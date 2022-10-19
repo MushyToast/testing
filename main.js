ef27b4d87e18db91cf2eaacdba59b75151551cae
galactic_ <102072394+MushyToast@users.noreply.github.com>
2022-10-18 17:51:19 -0700

    u mad goofy LMAO

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 window.onload = function(){
     document.getElementById("btn").onclick=function(){
         var text = document.getElementById("h1")
-        alert("You clicked me. Yay...");
-        text.innerHTML = "Goofy :skull:";
+        alert("You clicked me. Yay...")
+        text.innerHTML = "Goofy :skull:"
     }
+}
+
+var sbmit = document.getElementById("amogsus2")
+var kewltext = document.getElementById("amogsus")
+
+sbmit.onclick=function(){
+    text.innerHTML = toString(kewltext.Value)
 }
