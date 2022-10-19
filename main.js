6067e9909f4d0050568aec44141bf0169204c04f
galactic_ <102072394+MushyToast@users.noreply.github.com>
2022-10-18 17:34:13 -0700

    fixityfixityfixfixfix

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var text = document.getElementById("h1")
 window.onload = function(){
     document.getElementById("btn").onclick=function(){
+        var text = document.getElementById("h1")
         alert("You clicked me. Yay..."); //GOOFY AHH UNCLE PRODUCTIONS
         text.innerHTML = "Goofy :skull:";
     }
