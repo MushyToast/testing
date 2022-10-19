8a716fcffbdd67e8f0a515c3e0f3d08fbcf3a9d2
galactic_ <102072394+MushyToast@users.noreply.github.com>
2022-10-18 17:29:32 -0700

    billy la bufanda

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+var text = document.getElementById("h1")
 window.onload = function(){
     document.getElementById("btn").onclick=function(){
         alert("You clicked me. Yay..."); //GOOFY AHH UNCLE PRODUCTIONS
-        
+        text.innerHTML = "Goofy :skull:";
     }
 }
