2ccc94b1a8311d5195bbd07742c36b48b65ce4ce
galactic_ <102072394+MushyToast@users.noreply.github.com>
2022-10-18 17:59:35 -0700

    goofy ahh shmuncle productions

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 window.onload = function(){
     document.getElementById("btn").onclick=function(){
         var text = document.getElementById("h1")
-        alert("You clicked me. Yay...")
+        alert("Caught in 9k ayo")
         text.innerHTML = "sussy boi"
     }
     var sbmit = document.getElementById("amogsus2")
