113ccca98874d263a0e1328b0019267fe1b2515d
galactic_ <ndg72609@gmail.com>
2022-10-27 21:42:29 -0700

    goof

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 window.onload = function(){
     var text = document.getElementById("h1")
     document.getElementById("btn").onclick=function(){
-        alert("Caught in 9k ayo")
+        alert(document.getElementById("amogsus").value)
         text.innerHTML = "sussy boi"
     }
     document.getElementById("amogsus2").onclick = function(){
         document.getElementById("h1").innerHTML = document.getElementById("amogsus").value
     }
+    document.getElementById("clear").onclick = function(){
+        document.write(fetch("https://mushytoast.github.io/mysite/index.html"))
+    }
 }
-//chingchongbingbongsuckonmypingpong
+
 
 
 
