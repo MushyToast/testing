f989645b1bd848d1db786bd1b1141d108ee0e279
galactic_ <ndg72609@gmail.com>
2022-10-29 13:18:48 -0700

    kool

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
     var h1 = document.getElementById("women")
     document.getElementById('men').onclick = function(){
-        $("#women").animate({
-            duration: '5',
-            opacity: '0'
-        })
+        $("#women").animate({duration: '5',opacity: '0'
+    }, 100)
     }
 })
