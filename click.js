39be95d3fe6c28533ba37b611ff3a057e117db1d
galactic_ <102072394+MushyToast@users.noreply.github.com>
2022-11-09 12:30:36 -0800

    bust on me pls

--- click.js
+++ click.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
     const button = document.getElementById("bobux")
+    const button2 = document.getElementById("xtra")
+    const button3 = dcoument.getElementById("xxtra")
     const count = document.getElementById("count")
     let clicks = 0
     $(button).click(function(){
         clicks += 1
+        count.innerHTML = clicks
+    })
+    $(button2).click(function(){
+        clicks += 10
+        count.innerHTML = clicks
+    })
+    $(button3).click(function(){
+        clicks += 50
         count.innerHTML = clicks
     })
 })
