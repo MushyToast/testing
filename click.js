89b0067e13d30893a0e9798ce80ce5d2c9006210
galactic_ <102072394+MushyToast@users.noreply.github.com>
2022-11-09 11:48:05 -0800

    Reid Lewis Quandale Dingle Miller

--- click.js
+++ click.js
@@ -1,9 +1,9 @@
-var clicks = 0
 $(document).ready(function(){
     const button = document.getElementById("bobux")
     const count = document.getElementById("count")
+    let clicks = 0
     $(button).click(function(){
-        clicks = clicks + 1
+        clicks++
         count.innerHTML = toString(clicks)
     })
 })
